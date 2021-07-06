46609722dee1a911239839c0df4b3ebd22dba7cd
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-07-06 11:28:59 -0500

    Update

--- app/Http/Controllers/Page/HomeController.php
+++ app/Http/Controllers/Page/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Page;
 
 use App\Http\Controllers\Controller;
+use App\Models\TPaquete;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+        $paquete = TPaquete::with('paquetes_destinos.destinos','paquetes_categoria.categoria', 'precio_paquetes')->get();
+        dd($paquete);
         return view('page.home');
     }
 }
